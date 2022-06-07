c4cc639d9d46dbb11a135f5069e43b5fd86f111e
7Error228 <107021727+7Error228@users.noreply.github.com>
2022-06-07 13:02:21 +0500

    Yangi fayllar qo'shildi
    14-15darslarni javoblari

---
+++ dars-14 javobi.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 10:50:42 2022
+
+@author: 7Error228
+"""
+"""
+Dars - 14, Lug\'at bilan tanishuzv
+""" 
+# car_0 = {'model':'ferrari', 'rang':'qizil'}
+# print(car_0['model'])
+# print(car_0['rang'])
+
+# talaba_0 = {'ism':'Anvar Xasanov', 'yosh':15, 't_yil':'2007'}
+# print(f"{talaba_0['ism'].title()},\
+#   {talaba_0['t_yil']}-yilda tu'gilgan,\
+#   {talaba_0['yosh']} yoshda")
+# talaba_0['kurs'] = 4
+# talaba_0['fakultet'] = 'informatika'
+# print(talaba_0)
+# talaba_1 = {}
+# talaba_1['ism'] = 'qobil rasulov'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 21
+# print(talaba_1)
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']} - kurs")
+
+# talaba_0 = {'ism':'murod olimov','yosh':20,'t_yil':2000}
+# print(talaba_0)
+# del talaba_0['yosh'] # yosh degan kalit so'z (va qiymatni) o'chiramiz
+# print(talaba_0)
+
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+# phone = telefonlar['ali']
+# print(f"Alining telefoni {phone}")
+
+#AMALIYOT
+# ota = {'ism':'akbar', 'yosh':39, 't_yil':1983, 'sev_taom':'chuchvara'}
+# ona = {'ism':'dilnoza', 'yosh':37, 't_yil':1985,'sev_taom':'shorva' }
+# uka = {'ism':'Xasan', 'yosh':9, 't_yil':2012, 'sev_taom':'jarkob'}
+# print(f"Otamning ismi {ota['ism'].title()},{ota['t_yil']}-yilda tug\'lgan, {ota['yosh']} yoshda")
+# print(f"Onamning ismi {ona['ism'].title()},{ona['t_yil']}-yilda tug\'lgan, {ona['yosh']} yoshda")
+# print(f"Ukamning ismi {uka['ism'].title()},{uka['t_yil']}-yilda tug\'lgan, {uka['yosh']} yoshda")
+# singil = {'ism':'Zuxra', 'sev_taom':'osh'}
+# print(f"{uka['ism'].title()}ning sevimli taomi {uka['sev_taom']}")
+# print(f"{ota['ism'].title()}ning sevimli taomi {ota['sev_taom']}")
+# print(f"{ona['ism'].title()}ning sevimli taomi {ona['sev_taom']}")
+# print(f"{singil['ism'].title()}ning sevimli taomi {singil['sev_taom']}")
+
+
+# izoh = {
+#         'integer':"Butun son",
+#         'float':"O\'nlik son",
+#         'string':"Matn",
+#         'list':"Ro\'yxat",}
+# print(izoh)
+
+# soz = input("Kalit soz kiriting: ").lower()
+# print(izoh.get(soz, "Bunday soz mavjud emas!"))
+
+# soz1 = input("Kalit so\'z kiriting: ")
+# tarjima = izoh.get(soz1)
+# if tarjima==None:
+#     print("Bunday so\'z mavjud emas!")
+# else:
+#     print(f"{soz1.title()} so\'zi tarjimasi {tarjima}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dars-15 javobi.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 11:53:39 2022
+
+@author: user
+dars-15, Lug\'at elementlari bilan ishlash
+"""
+
+# talaba_0 = {
+#     'ism':'alijon',
+#     'familiya':'shamshiyev',
+#     'yosh':22,
+#     'fakultet':'matematika',
+#     'kurs':4
+#     }
+# print(talaba_0.items())
+# for kalit, qiymat in talaba_0.items():
+#     print(f"Kalit: {kalit}")
+#     print(f"Qiymat: {qiymat} \n")
+    
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310'
+#     }
+# for k, q in telefonlar.items():
+#     print(f"{k.title()}ning telefoni {q}")
+
+#AMALIYOT
+# izoh = {
+#         'if':"Shartlarni tekshirish operatori",
+#         'boolean':"Mantiqiy qiymat",
+#         'float':"O\'nlik son",
+#         'for':"Biror amalni qayta-qayta bajarish tsikli",
+#         'integer':"Butun son"}
+# for kalit in sorted(izoh):
+#     print(f"{kalit.title()}")
+
+# davlatlar = {
+#     "o'zbekiston":'toshkent',
+#     'aqsh':'washington d.c.',
+#     'rossiya':'moskva',
+#     'tojikiston':'dushanbe',
+#     "qirg'iziston":'bishkek',
+#     'qozog\'iston':'nursulton',
+#     'malayziya':'kuala-lumpur',
+#     'singapur':'sungapur',
+#     'italiya':'rim'}
+
+# print('Dunyo davlatlari:')
+# for davlat in sorted(davlatlar):
+#     print(davlat.upper())
+
+# print('\nDavlatlarning poytaxtlari')
+# for poytaxt in sorted(davlatlar.values()):
+#     print(poytaxt.title())
+
+# country = input('Qaysi davlatning poytaxtini bilishni istaysiz?:').lower()
+# capital = davlatlar.get(country)
+# if capital==None:
+#     print('Kechirasiz, bizda bu haqida ma\'lumot yo\'q')    
+# else:
+#     print(f"{country.upper()}ning poytaxti {capital.title()} shahri")
+
+# menu = {
+#         'osh':20000,
+#         "lag'mon":22000,
+#         'non':4000,
+#         'choy':5000,
+#         'shashlik':12000,
+#         'somsa':6000,
+#         'tabaka':15000
+#         }
+# print('3 ta taon buyurtma bering.')
+# buyurtmalar = []
+# for n in range(3):
+#     buyurtmalar.append(input(f"{n+1}-taom: ")).lower()
+    
+# for buyurtma in buyurtmalar:
+#     if buyurtma in menu:
+#         print(f"{buyurtma.title()} {menu[buyurtma]} so'm")
+#     else:
+#         print(f"Kechirasiz, bizda {buyurtma} yo'q.")
+
+
+
+
+
+
+
+
