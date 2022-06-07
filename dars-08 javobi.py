1736513de62d00916b903c111d41f265f54ed73a
7Error228 <KhasanovAnvar07@gmail.com>
2022-06-07 10:42:44 +0500

    Yangi fayllar qo'shildi
    8-9 darslarni javoblari

---
+++ dars-08 javobi.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  3 09:31:41 2022
+
+@author: 7Error228
+"""
+#AMALIYOT
+# davlatlar = ['Ozbekiston', 'Turkiston', 'Russia', 'Koreya', 'Xitoy']
+# print(len(davlatlar))
+# print(sorted(davlatlar))
+# print(davlatlar)
+# print(sorted(davlatlar, reverse=True))
+# print(davlatlar)
+# print(sorted(davlatlar, reverse=False))
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+
+# royxat = list(range(120,1201,2))
+# print(len(royxat))
+# kichik = min(royxat)
+# katta = max(royxat)
+# print("Eng katta son va eng kichik sonni ayirmasi =", katta - kichik)
+# jami = sum(royxat)
+# print(jami)
+# print(royxat[:20])
+# print(royxat[225:250])
+# print(royxat[520:540])
+    
+# taomlar = ['chuchvara', 'dimlama', 'qaynatma', 'lag\'mon', 'osh'] 
+# nonushta = taomlar[:]
+# nonushta.append('tuxum')
+# nonushta.append('issiq non')
+# nonushta.remove('dimlama')
+# nonushta.remove('qaynatma')
+# nonushta.remove('lag\'mon')
+# print(taomlar)
+# print(nonushta) 
+# nonushta = tuple(nonushta)
+# nonushta[0] = "qaymoq"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dars-09 javobi.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 10:37:18 2022
+
+@author: 7Error228
+"""
+
+#AMALIYOT
+# ismlar = ['arsen', 'danil', 'farrux', 'ramiz', 'ozod']
+# for ism in ismlar:
+#     print(f"Salom {ism.title()}")
+# print(f"Kod {len(ismlar)} marta takrorlandi!")  
+
+# sonlar = list(range(1,100,2))
+# for son in sonlar:
+#     print(f"{son} ning kubi {son**3} ga teng")
+# kino = []
+# print("5 ta eng sevimli kinoingizni kiriting:")
+# for n in range(5):
+#     kino.append(input(f"{n+1}-kinoni kiriting:"))
+# print(kino)  
+
+# odam = input("Bugun necha kishi bilan suhbat qildingiz?>>>")
+# odam = int(odam)
+# suhbat = []
+# for o in range(odam):
+#     suhbat.append(input(f"{o+1}-suhbar qilgan odamingizni kiriting:"))
+# print(suhbat)   
